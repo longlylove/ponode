7b0760e2241350b34659de913ce4338be83ed56d
William Le <william.le@xero.com>
2016-07-13 14:16:48 +1200

    Spying on review

--- test/first_test.js
+++ test/first_test.js
@@ -1,7 +1,9 @@
 var assert = require("assert");
+
 describe ("A feature", function (){
 	describe("A scenario", function(){
 		it("does something expected", function(){
+			//TODO: fix this !!
 			assert(true);
 		});
 	});
